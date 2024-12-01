82b8fe873b15948aca1fa0645d784abd8fb71661
Babab-23 <adrianidialu@gmail.com>
2024-12-01 20:44:48 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+#Print a sentence
+print("Wekcome to programming.")
+
+#Print a number
+print(4)
+
+#Next line after print
+print("Hello Python \n")
+ 
+ #Pas multiple arguments for printing
+print("hello", 5)
+
+#End argument of Print statement
+print("Welcome to", end= "*" )
