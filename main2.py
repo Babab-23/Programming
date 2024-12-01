1733b0fb0833b03f5eb39ca10e076109392ad624
Babab-23 <adrianidialu@gmail.com>
2024-12-01 20:55:58 +0100

    second commit

---
+++ main2.py
@@ -0,0 +1,12 @@
+# Creating different variables
+x= 5
+y="Adrian"
+print(x)
+print(y)
+
+codingal="codingal"
+print(codingal)
+
+#User Input
+name=input("enter your name: ")
+print("\n Hello", name, "\n Welcome to Codingal")
