71bbd2cf9b72039e3b9dfecd2034d3489dc8ae77
Babab-23 <adrianidialu@gmail.com>
2024-12-03 19:11:01 +0100

    fourth commit

---
+++ main4.py
@@ -0,0 +1,19 @@
+from datetime  import date
+# Storing birthdays in different variables 
+birthday1=date(2010, 8, 23)
+# Example1: August 23,2010
+birthday2=date(1977, 3, 3)
+# Example2: March 3,1977
+birthday3=date(1977, 12, 13)
+# Example3: December 13,1977
+birthday4=date(2012, 7, 16)
+# Example4: July 16,2012
+birthday5=date(2014, 4, 4)
+# Example5: April 4,2014
+# Display the birthdays
+print("Stored  Birthdays: ")
+print("Birthday 1: {birthday1}")
+print("Birthday 2: {birthday2}")
+print("Birthday 3: {birthday3}")
+print("Birthday 4: {birthday4}")
+print("Birthday 5: {birthday5}")
